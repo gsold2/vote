47d647df566304b26e679aa374e84aeb4137de8f
Grigorii <grigoriisoldatov>
2023-10-21 13:17:19 +0300

    add annotation

--- src/main/java/ru/bootjava/vote/util/VoteUtil.java
+++ src/main/java/ru/bootjava/vote/util/VoteUtil.java
@@ -1,11 +1,13 @@
 package ru.bootjava.vote.util;
 
+import lombok.experimental.UtilityClass;
 import ru.bootjava.vote.model.Vote;
 import ru.bootjava.vote.to.VoteTo;
 
 import java.util.Collection;
 import java.util.List;
 
+@UtilityClass
 public class VoteUtil {
     public static List<VoteTo> getTos(Collection<Vote> votes) {
         return Converter.convert(votes, VoteUtil::createTo);
